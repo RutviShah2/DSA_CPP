16316650626e118da7987cc3f2110a1c3d186825
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-06 12:52:29 +0530

    initial commit

--- Array/insertBegin.cpp
+++ Array/insertBegin.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int element=60;
+    vector <int>arr= {10,20,30,40,50};
+    cout<<"Before insertion: ";
+    for (int i=0; i<5; i++)
+    {
+    cout<<arr[i]<<endl;
+    }
+    arr.insert(arr.begin(),element);
+    cout<<"After insertion: ";
+    for (int i=0; i<6; i++)
+    {
+    cout<<arr[i]<<endl;
+    }
 
+    
+}
