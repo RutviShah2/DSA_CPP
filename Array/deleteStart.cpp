8db8aea7d090e2b936d6a67f5700df4624adddee
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-14 18:39:57 +0530

    initial commit

---
+++ Array/deleteStart.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+int element=10;
+vector <int> arr = {10, 20, 30, 40,50,};
+cout<<"Before Deletion"<<endl;
+for (int i=0; i<arr.size(); i++)
+{
+    cout<< arr[i]<<endl;
+}
+
+    auto it = find(arr.begin(), arr.end(), element);
+    if (it != arr.end()) {
+        arr.erase(it);
+    }
+
+cout<<"After Deletion"<<endl;
+for (int i=0; i<arr.size(); i++)
+{
+    cout<< arr[i]<<endl;
+}
+return 0;
+}
