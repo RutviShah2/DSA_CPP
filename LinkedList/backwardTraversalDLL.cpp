62f12c2edfb6e27e88174f63d4fd5df56bc10352
Rutvi Shah <shahrutvi020@gmail.com>
2025-07-22 22:42:20 +0530

    Update backwardTraversalDLL.cpp

--- LinkedList/backwardTraversalDLL.cpp
+++ LinkedList/backwardTraversalDLL.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
-
 struct Node {
     int data;
     Node *next;
     Node *prev;
-
     Node(int val) {
         data = val;
         next = nullptr;
@@ -24,14 +22,11 @@ int main() {
     Node *head = new Node(1);
     Node *second = new Node(2);
     Node *tail = new Node(3);
-
     tail->prev = second;
     second->next = tail;
     second->prev =head ;
     head->next = second;
-
     cout << "Backward Traversal: ";
     backwardTraversal(tail);
-
     return 0;
 }
