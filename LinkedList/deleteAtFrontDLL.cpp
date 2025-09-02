990c31086067049eb50d1257d5c74792f2d0c92a
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-02 16:34:36 +0530

    IMP

---
+++ LinkedList/deleteAtFrontDLL.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* prev;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+void insertAtFront(Node* &head, int val) {
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    if (head != nullptr) head->prev = newNode;
+    head = newNode;
+}
+
+void deleteAtFront(Node* &head) {
+    if (head == nullptr) {
+        cout << "List is empty, nothing to delete!" << endl;
+        return;
+    }
+
+    Node* temp = head;
+    head = head->next;
+    if (head != nullptr) head->prev = nullptr;
+    delete temp;
+}
+
+void display(Node* head) {
+    cout << "Doubly Linked List: ";
+    while (head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = nullptr;
+
+    insertAtFront(head, 10);
+    insertAtFront(head, 20);
+    display(head); 
+    deleteAtFront(head);
+    display(head);      
+    deleteAtFront(head); 
+    display(head);    
+    return 0;
+}
